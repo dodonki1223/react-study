72bc30e56195d75e3ea2b9342160c03c4f72bd50
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-12-14 09:16:02 +0900

    :sparkles: 8-3. クラスコンポーネントで学ぶ state - 3

--- Riakuto-StartingReact-ja3.0/08-component/03-state/02-state/src/App.tsx
+++ Riakuto-StartingReact-ja3.0/08-component/03-state/02-state/src/App.tsx
@@ -6,9 +6,18 @@ type State = {
   count: number;
 };
 
+/*
+    - Component<unknown, State> と型引数が２つある
+      - unknown の部分が props だが今回は必要ないので unknown で定義している
+      - @typescript-eslint/ban-types のルールで仕様が禁じられている
+      - プロパティを持てない unknown がここではふさわしい
+    - ２つ目の引数が state の型になる
+ */
 class App extends Component<unknown, State> {
   constructor(props: unknown) {
+    // この記述はお約束で必要である（親クラスのコンストラクタを props を使用して実行 ）
     super(props);
+    // 直接値を代入していいのは constructor の中だけである
     this.state = { count: 0 };
   }
 
@@ -17,6 +26,12 @@ class App extends Component<unknown, State> {
   }
 
   increment(): void {
+    /*
+        「this.setState({ count: state.count + 1 })」の書き方では駄目なの？
+          - クラスの state の更新は React によるレンダリング最適化処理の中で非同期に行われる
+          - increment() が参照する state の値はその瞬間での最新の値であることが厳密には保証されない
+          - state の前の状態に依存するような変更処理は (state, props) => ({ foo: state.foo + props.bar }) の書き方を使用したほうがよい
+     */
     this.setState((state) => ({ count: state.count + 1 }));
   }
 
@@ -38,6 +53,15 @@ class App extends Component<unknown, State> {
               <Button color="red" onClick={() => this.reset()}>
                 Reset
               </Button>
+              {/*  
+                  - () => this.increment() ・・・ 引数を受けとらずに increment メソッドを実行する無名関数
+
+                  - ユーザーのフォーム操作によって、親コンポーネントの中身を書き換える必要がある時はどうするのか？
+                    - 親コンポーネントが自身の状態を変更する関数を子コンポーネントに渡してフォーム入力時発火される
+                      イベントへその関数を仕込んでおく
+                    - 「() => this.increment()」が親コンポーネント自体の値を更新している
+                    - 「onClick={() => this.increment()}」が子コンポーネントに渡してフォーム入力時発火されるイベントへ関数を仕込んでいる
+              */}
               <Button color="green" onClick={() => this.increment()}>
                 +1
               </Button>
