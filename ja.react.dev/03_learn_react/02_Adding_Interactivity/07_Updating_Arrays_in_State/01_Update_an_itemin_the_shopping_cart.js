00f8d765d78afbbc3ef89a0a2b0eacd265427c69
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2023-11-27 22:59:55 +0000

    ショッピングカードの商品を更新

---
+++ ja.react.dev/03_learn_react/02_Adding_Interactivity/07_Updating_Arrays_in_State/01_Update_an_itemin_the_shopping_cart.js
@@ -0,0 +1,107 @@
+/*
+1. ショッピングカードの商品を更新
+
+handleIncreaseClick のロジックを埋めて、”+” を押すことで対応する数字が増えるようにしてください。
+
+import { useState } from 'react';
+
+const initialProducts = [{
+  id: 0,
+  name: 'Baklava',
+  count: 1,
+}, {
+  id: 1,
+  name: 'Cheese',
+  count: 5,
+}, {
+  id: 2,
+  name: 'Spaghetti',
+  count: 2,
+}];
+
+export default function ShoppingCart() {
+  const [
+    products,
+    setProducts
+  ] = useState(initialProducts)
+
+  function handleIncreaseClick(productId) {
+
+  }
+
+  return (
+    <ul>
+      {products.map(product => (
+        <li key={product.id}>
+          {product.name}
+          {' '}
+          (<b>{product.count}</b>)
+          <button onClick={() => {
+            handleIncreaseClick(product.id);
+          }}>
+            +
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+*/
+
+
+/*
+map 関数を使って新しい配列を作成し、その中で ... オブジェクトスプレッド構文を
+使って変更された商品に対応するオブジェクトのコピーを作成します
+*/
+import { useState } from 'react';
+
+const initialProducts = [{
+  id: 0,
+  name: 'Baklava',
+  count: 1,
+}, {
+  id: 1,
+  name: 'Cheese',
+  count: 5,
+}, {
+  id: 2,
+  name: 'Spaghetti',
+  count: 2,
+}];
+
+export default function ShoppingCart() {
+  const [
+    products,
+    setProducts
+  ] = useState(initialProducts)
+
+  function handleIncreaseClick(productId) {
+    setProducts(products.map(product => {
+      if (product.id === productId) {
+        return {
+          ...product,
+          count: product.count + 1
+        };
+      } else {
+        return product;
+      }
+    }))
+  }
+
+  return (
+    <ul>
+      {products.map(product => (
+        <li key={product.id}>
+          {product.name}
+          {' '}
+          (<b>{product.count}</b>)
+          <button onClick={() => {
+            handleIncreaseClick(product.id);
+          }}>
+            +
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
